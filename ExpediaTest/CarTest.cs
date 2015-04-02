b1b0e49db789af9458269a7f6186d5466413bcda
Doolan <doolansr@rose-hulman.edu>
2015-04-02 14:13:07 -0400

    Some Testing

--- ExpediaTest/CarTest.cs
+++ ExpediaTest/CarTest.cs
@@ -50,5 +50,36 @@ namespace ExpediaTest
 		{
 			new Car(-5);
 		}
+
+
+        [TestMethod]
+        public void TestgetCarLocation()
+        {
+            IDatabase mockDB = mocks.DynamicMock<IDatabase>();
+
+            String carLocal1 = "The Spot";
+            String carLocal2 = "The Other Spot";
+            using (mocks.Ordered())
+            {
+                Expect.Call(mockDB.getCarLocation(24)).Return(carLocal1);
+                Expect.Call(mockDB.getCarLocation(1025)).Return(carLocal2);
+                mockDB.Stub(x => x.getCarLocation(Arg<int>.Is.Anything)).Return("Uknown");
+            }
+            mocks.ReplayAll();
+
+            Car target = new Car(10);
+            target.Database = mockDB;
+
+            String result;
+
+            result = target.getCarLocation(24);
+            Assert.AreEqual(carLocal1, result);
+            result = target.getCarLocation(1025);
+            Assert.AreEqual(carLocal2, result);
+            result = target.getCarLocation(25);
+            Assert.AreEqual("Uknown", result);
+
+            mocks.VerifyAll();
+        }
 	}
 }
